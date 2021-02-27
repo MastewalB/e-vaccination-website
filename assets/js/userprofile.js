9a88c29a24ae9f654ae7455f6220f2e94981d8a5
Meti A <metiade7@gmail.com>
2021-02-27 03:04:58 +0300

    page connected with vaccine db fetches custom  recommended vacines and available vaccines for each user

--- assets/js/userprofile.js
+++ assets/js/userprofile.js
@@ -1,9 +1,36 @@
 const logoutBtn = document.getElementById("logout");
 const brandTitle = document.querySelector("#brandTitle");
-const vaccineContainer = document.querySelector(".row .recommended_vaccines_container");
+const vaccineContainer = document.querySelector(".row.recommended_vaccines_container");
+
+const availableContainer = document.querySelector(".row.available_vaccines_container");
 const notification = document.querySelector("#notification");
 
 
+
+
+function vaccineTemplate(imgurl, description_string, title_string, vaccineid){ 
+    const template = 
+    `<div class="col-xl-4 d-flex align-items-stretch " data-id="${vaccineid}">
+    <div class="icon-box mt-4 mt-xl-0">
+    <span class="iconify" data-icon="bx-bxs-right-arrow" data-inline="false"></span>
+    <img src="${imgurl}" alt="" class="img-fluid">
+    <h4 id="title">${title_string}</h4>
+    <p id="desc">${description_string}</p>
+    <button class="btn btn-primary" id ="applyBtn">Apply for Vaccine</button>
+    </div>
+    </div>`
+
+    return template
+}
+
+function getAge(birthdate){
+    let currentDate = new Date()
+    var timestamp = Date.parse(birthdate);
+    var birthdate = new Date(timestamp);
+    const user_age = currentDate.getFullYear() - birthdate.getFullYear()
+    return user_age
+
+}
 
 
 db.transaction('rw', db.users, function () { 
@@ -19,6 +46,8 @@ db.transaction('rw', db.users, function () {
                 // *********************************************************
                 // display on to the user html page 
                 brandTitle.innerHTML = user.user.name
+                let user_age = getAge(user.user.birthdate)
+                checkVaccineUpdate(user_age)
                 console.log("Read from table and updated")
                 console.log(user.user.vaccine_alert.length)
             
@@ -31,3 +60,36 @@ db.transaction('rw', db.users, function () {
 }).catch(e =>{
     console.log("Error", e)
 })
+
+
+// *************************fetch data from vaccine database*************************************
+function checkVaccineUpdate(age){
+    dbv.transaction('rw',dbv.vaccines, function () { 
+        // sth
+    })
+    .then(function(){
+        dbv.vaccines.where("min_age")
+        .between(0, 85,true)
+        .each(vaccine => {
+            if (vaccine.min_age == age){
+                console.log("AGee check passed")
+                let template= vaccineTemplate("assets/img/departments-4.jpg", vaccine.description, vaccine.vaccine_name, vaccine.vaccine_name)
+                let recommendedRow = vaccineContainer.querySelector(".row")
+                recommendedRow.innerHTML += template
+            }else{
+
+                console.log("AGee check failed")
+                let template= vaccineTemplate("assets/img/departments-4.jpg", vaccine.description, vaccine.vaccine_name, vaccine.vaccine_name)
+                let availableRow = availableContainer.querySelector(".row")
+                availableRow.innerHTML += template
+                
+            }
+            console.log("Found: " + vaccine.vaccine_name);
+        }).catch(error => {
+            console.log(error.stack);
+        });
+
+    }).catch(e =>{
+        console.log("Error", e)
+    })
+}
