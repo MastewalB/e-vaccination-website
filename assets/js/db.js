423e8cec473124e8dd2b8598d89f939bafba4a60
Mastewal B <se.mastewal.belete@gmail.com>
2021-02-27 21:13:40 +0300

    Vaccine Session added to database

--- assets/js/db.js
+++ assets/js/db.js
@@ -3,7 +3,8 @@ db.version(1).stores({
     admin: '&username, admin',
     dependents: '&username, dependant',
     users: '&username, user',
-    workers: '&username, worker'
+    workers: '&username, worker',
+    vaccine_session: '++, vaccine_id'
 });
 
 db.open().catch(function (e) {
@@ -12,9 +13,9 @@ db.open().catch(function (e) {
 
 // **********************Vaccine database*********************
 var dbv = new Dexie("Vaccines");
-          dbv.version(3).stores({
-            vaccines: 'vaccine_name, min_age, description, active'
-          });
+dbv.version(3).stores({
+    vaccines: 'vaccine_name, min_age, description, active'
+});
 
 dbv.open().catch(function (e) {
     console.error("Open failed: " + e.stack);
