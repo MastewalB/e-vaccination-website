8df0d24bfff785598ca6a939d9b3c030d178fdb2
Mastewal B <se.mastewal.belete@gmail.com>
2021-02-27 18:08:35 +0300

    Utilites.js - Minor Improvements on JSDoc

--- assets/js/utilities.js
+++ assets/js/utilities.js
@@ -11,7 +11,7 @@ class Shift {
     }
 
 
-    /*
+    /** 
     * Returns doctors on a specified day of the week
     * @param {String} day - The day of the week
     * @returns {Array} - Doctors working on that day
@@ -39,7 +39,7 @@ class VaccineSession {
     day = new Map();
 
 
-    /*
+    /**
     * Creates a doctors object with id, tag, and patiens attribute after accepting their list from the Shift provider
     * @param {Array} array - This is the list of doctors
     * @return {Array} mapped - This array contains the list of Doctor objects with necessary attributes
@@ -60,7 +60,7 @@ class VaccineSession {
     }
 
 
-    /*
+    /**
     *Takes the day number(like day 06) of the vaccination session and returns the actual date
     * @param {Number} day_number_value - Is the nth day of the session
     * @returns {Array} day - Containing the day object in human readable form 
@@ -72,7 +72,7 @@ class VaccineSession {
         return ["Monday", "February/15/2021"];
     }
 
-    /*
+    /**
     *Takes day value like Monday from the shift class and returns doctors on that day
     * @param {String} day - Day of the week
     * @return {Array} doctors - By contacting the shift class, it obtains the doctors working on that day
