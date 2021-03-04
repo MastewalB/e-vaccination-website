5100b69cb70b48fc18a238bb3b186eda9f70ba17
Natnael Alemayehu <se.natnael.alemayehu@gmail.com>
2021-03-04 11:23:03 +0300

    API - the fetchNews function is under construction

--- assets/js/api.js
+++ assets/js/api.js
@@ -2,3 +2,12 @@ let postDiv3 = document.querySelector('#posts')
 const btn_search = document.querySelector("#search")
 const searchQuery = document.querySelector("#search_query")
 var url = 'http://newsapi.org/v2/top-headlines?country=us&category=health&apiKey=a862f33fff6545a994a4d8bce20be778';
+
+async function fetchNews(url) {
+    const response = await fetch(url, {
+      mode: 'cors',
+    });
+    const news = await response.json()
+    return news;
+  
+  }
