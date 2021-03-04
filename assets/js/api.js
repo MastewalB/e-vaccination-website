a32d0684d9bda94d440a3b0caa14fb81aa438b4f
Mastewal B <se.mastewal.belete@gmail.com>
2021-03-04 11:09:58 +0300

    API - api.js Querying elements from the html

---
+++ assets/js/api.js
@@ -0,0 +1,4 @@
+let postDiv3 = document.querySelector('#posts')
+const btn_search = document.querySelector("#search")
+const searchQuery = document.querySelector("#search_query")
+var url = 'http://newsapi.org/v2/top-headlines?country=us&category=health&apiKey=a862f33fff6545a994a4d8bce20be778';
