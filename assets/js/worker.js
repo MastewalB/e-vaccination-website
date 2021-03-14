67f09618294e769dbf6e2810b51a9a30a22e1651
Meti A <metiade7@gmail.com>
2021-03-14 11:10:06 +0300

    connecting doctors page with session databse

--- assets/js/worker.js
+++ assets/js/worker.js
@@ -20,7 +20,32 @@ function createTaskRecordTemplate(){
   </tr>`
 
 }
-
+//  fetch 2 
+sessiondb.transaction('rw',sessiondb.sessions, function () { 
+    // sth
+  })
+  .then(function(){
+    sessiondb.sessions.where("assigned_doctor_id","session_status")
+    // .between(vaccine.min_age, vaccine.min_age+ 10,true)
+    .equals(localStorage.getItem("currentDoctor"),"Complete",true, true)
+    .each(session => {
+      console.log(session.patientId, session.patient_info.appointemntDate) 
+      tasksTable.innerHTML += `
+      <tr>
+      <th scope="row">${0}</th>
+      <td id="patient_id_field">${session.patientId}</td>
+      <td>${session.vaccine_ID}</td>
+      <td id="scheduledDate">${session.patient_info.appointemntDate}</td>
+      <td><button type="button" class="btn btn-primary btn-block btn-sm" id="patientInfo ${session.patientId}" onclick="viewMedical()">Patient Info</button></td>
+      <td>
+          <button class="btn btn-success btn-sm btn-block statusBtn" onclick="setStatus()">Complete</button>
+      </td>
+    </tr>`;
+    });
+  
+  }).catch(e =>{
+    console.log("Error", e)
+  })
 // update Worker page
 db.transaction('rw', db.workers, function () { 
     // sth
