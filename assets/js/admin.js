87a61eb529c57669d3c1b939ace389f413620e6c
Meti A <metiade7@gmail.com>
2021-03-15 12:15:24 +0300

    fetch doctors data from workers database and populate it on the admin page

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -40,3 +40,99 @@ addBtn.addEventListener("click", ()=>{
   close.addEventListener('click', () =>{
     popup.style.display="none";
   });
+
+  function generateWorkerID(){
+    return 'st' + Math.random().toString(36).substr(2, 7);
+  }
+  function generateWorkerpassword(){
+    return '' + Math.random().toString(6).substr(2, 4);
+  }
+  function createWorker(){
+    let person = new Worker()
+    person.name = workername.value;
+    person.phonenumber = workerphone.value;
+    person.birthdate = workerbirthdate.value;
+    person.password = generateWorkerpassword();
+    person.email = person.name+"@ykstuff.com";
+    person.gender = workergender.value
+    person.subcity = workersubcity.value
+  
+    person.department = workerdepartment.value;
+    person.worker_id = generateWorkerID();
+    person.tag = 0;
+    person.patientInfo = {}
+  
+    return person
+  }
+  addWorker.addEventListener("submit", e=>{
+    e.preventDefault()
+    let person = createWorker()
+    db.transaction('rw', db.workers, function () { 
+      // sth
+    }).then(function(){
+        db.workers.put({
+            username: person.email, 
+            worker: person
+          }).then(function(){
+              console.log("Worker Added")
+              location.reload()
+              alert(`worker Added\n Email ${person.email}\n password ${person.password}\n Worker ID${person.worker_id}`)
+  
+  
+              // populateWorker()
+            //    login()
+          }).catch(e =>{
+              console.log("Error", e)
+          })
+        
+    }).catch (function (e) {
+        console.error(e.stack);
+        
+    });
+  // create 
+  })
+  
+  function createRecordTemplate(worker_name, worker_id, active, worker_email, worker_gender){
+    output = `
+    <tr>
+    <th scope="row"></th>
+    <td>${worker_name}</td>
+    <td>${worker_id}</td>
+    <td>${active}</td>
+    <td><button type="button" class="btn btn-primary btn-block btn-sm" id="workerDetail">worker Info</button></td>
+    
+    
+   
+  </tr>`
+  return output
+}
+
+// populate the stuff list from the workers database
+db.workers.orderBy('username').eachPrimaryKey(function (primaryKey) {
+
+  console.log(primaryKey)
+  db.workers.get({username:primaryKey}, user=>{
+      return user
+     
+     }).then(function(user ){
+      if (user){
+        row.innerHTML += createRecordTemplate(user.worker.name, user.worker.worker_id, "active",user.worker.email,user.worker.gender)         
+      
+     }
+  
+}).catch(e =>{
+  console.log("Error", e)
+})
+
+});
+
+
+// show/hide worker details
+row.addEventListener("click", e=>{
+
+  if (e.target.id == "workerDetail"){
+    detailsParent = e.target.parentElement.parentElement
+    detailsDiv = detailsParent.nextSibling
+    detailsDiv.classList.toggle("hiddenView")
+}
+})
